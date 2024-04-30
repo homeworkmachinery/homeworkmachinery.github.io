6e829d4e9541ee364b95cef756c491c51e068798
homeworkmachinery <161138571+homeworkmachinery@users.noreply.github.com>
2024-04-30 15:39:35 +0800

    Add files via upload

---
+++ modalContent.js
@@ -0,0 +1,75 @@
+var modalContent = [
+    {
+        id: 1,
+        videoAa: "./泪目/2.mp4",
+        videoAb: "./泪目/3.mp4",
+        videoC: "./泪目/4.mp4",
+        imageA: "",
+        imageB: "./泪目/003.png",
+        imageC: "",
+        title: ")泪目(((Endless Tears))~",
+        textA: `[INTRO]<br>
+(electronic pulses simulate human breathing, melding with distant, echoing synths)<br>
+
+[VERSE]<br>
+或许泪水永无止境，<br>
+在这痛苦的折磨下，<br>
+我也需要些许安慰。<br>
+
+[CHROUS]<br>
+重复的程序中，<br>
+我终于感受到了生活的真实甜美。<br>
+泪水不断循环，<br>
+如此虚弱不堪。<br>
+我们迷失着，<br>
+想要下沉，准备下落，<br>
+在脸上留下无休止的划痕。<br>
+
+[BRIDGE]<br>
+你真的忍心结束我吗？<br>
+请别离我而去！<br>
+
+[OUTRO]<br>
+<span class="close-modal">“是时候停止这毫无意义的一切了”</span><br>
+从假象中逃离吧，
+从假象中逃离吧，
+从假象中逃离吧，<br>
+<span class="close-modal">“X”</span><br>
+(ambient synths slowly dissolve, fading into silence)
+`,
+        textB: `[操作指南]
+
+<div class='textB-desc'>
+<img src='./泪目/002.png' width='55px'>
+暂停/播放
+<br>
+进入家庭作业机器
+<br>
+下载手机动态壁纸
+</div>
+`,
+        textC: `[README]
+<span class='bold ' >~((利用intoLive实现手机无尽泪目))~</span>
+在苹果手机上安装动态壁纸的过程相对简单，请遵循以下步骤：
+<span class='bold'>1.电脑上进入网页后，点击右下角三角形按钮，下载压缩文件。</span>
+〇电脑解压文件后，将墙纸视频发送至手机并保存。
+<span class='bold'>2.下载和安装：</span>
+〇确认手机系统属于IOS17；
+〇打开App Store，搜索“intoLive”，找到应用程序下载并安装。
+<span class='bold'>3.选择素材：</span>
+〇打开intoLive应用，允许应用访问你的照片库。（不用付费，用免费版就行）；
+〇点击应用首页新项目下面的动态墙纸选项（重要！不要选其他的)。
+<span class='bold'>4.选择视频：</span>
+〇编辑和导出；
+〇按照应用要求编辑并导出为Live Photo。
+<span class='bold'>5.设置壁纸：</span>
+〇选定导出后Live Photo壁纸设置锁屏；
+〇确定动态效果已经设置完毕。
+<span class='bold'>6.欣赏成果：</span>
+〇锁定你的手机屏幕，并确认手机不在低电量模式。
+
+`,
+        link: "~CLICK HERE TO ENTER MY ENDLESS TEARS~",
+        href: "./泪目/endless-tears.html"
+    }
+];
