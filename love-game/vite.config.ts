9773ec5d82d7552e7133480ce74d6afaf4aa075d
Your Name <your-email@example.com>
2024-12-08 18:24:32 +0800

    Delete files and update love-game

--- love-game/vite.config.ts
+++ love-game/vite.config.ts
@@ -2,19 +2,12 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+
   build: {
+    outDir: 'dist',  // 输出目录
     rollupOptions: {
-      input: {
-        // 确保入口文件是 scene-1.ts
-        main: path.resolve(__dirname, 'index.html'),
-      },
+      input: path.resolve(__dirname, 'index.html'),  // 设置入口文件
     },
-    assetsInlineLimit: 0, // 防止将大于 0 的资源内联
-   
-  },
-  server: {
-    port: 3000,
-    open: true,
   },
   publicDir: 'public',  // 指定 public 文件夹
   optimizeDeps: {
